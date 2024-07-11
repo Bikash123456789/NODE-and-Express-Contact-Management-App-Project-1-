04aaae2f7d04fc5f05b03435d0882b0ccd165ec0
Bikash <medhi01011997@gmail.com>
2024-07-11 18:42:40 +0530

    Part 6: Multiple http methods per route

--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -3,14 +3,8 @@ const express = require("express");
 const router = express.Router()
 const {getContacts,getContact,createContact,updateContact,deleteContact} = require("../controllers/contactController")
 
-router.get("/",getContacts)
+router.route("/").get(getContacts).post(createContact)
 
-router.post("/",createContact)
-
-router.get("/:id",getContact)
-
-router.put("/:id",updateContact)
-
-router.delete("/:id",deleteContact)
+router.route("/:id").get(getContact).put(updateContact).delete(deleteContact)
 
 module.exports = router
