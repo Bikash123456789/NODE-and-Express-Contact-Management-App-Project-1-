fd517ac9e7b8d77e37f1bc0daef636e2093f1545
Bikash <medhi01011997@gmail.com>
2024-07-11 17:43:54 +0530

    Part 4(b): Add post,put and delete routes inside the contactRoutes.js file

--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -5,4 +5,20 @@ router.get("/",(req,res)=>{
     res.send({message:"Get all users"})
 })
 
+router.post("/",(req,res)=>{
+    res.send({message:"Create a new user"})
+})
+
+router.get("/:id",(req,res)=>{
+    res.send({message:`Get the user ${req.params.id}`})
+})
+
+router.put("/:id",(req,res)=>{
+    res.send({message:`Update the user ${req.params.id}`})
+})
+
+router.delete("/:id",(req,res)=>{
+    res.send({message:`Delete the user ${req.params.id}`})
+})
+
 module.exports = router
