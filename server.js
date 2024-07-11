0e72146e704273293af0926a81102cbb5c63cd94
Bikash <medhi01011997@gmail.com>
2024-07-11 13:49:14 +0530

    Part 2 : Creating a simple Express Server

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express")
+const dotenv = require('dotenv').config()
+
+
+const app = express()
+
+
+const PORT = process.env.PORT || 5000
+
+
+app.listen(PORT,()=>{
+    console.log(`Server listening at ${PORT}`)
+})
