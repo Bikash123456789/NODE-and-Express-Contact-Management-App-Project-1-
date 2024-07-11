739ac662adba094df61f28993931dea3ca155db7
Bikash <medhi01011997@gmail.com>
2024-07-11 13:59:45 +0530

    Part 3: Creating a simple route and setting up Thunder Client

--- server.js
+++ server.js
@@ -4,8 +4,18 @@ const dotenv = require('dotenv').config()
 
 const app = express()
 
-
 const PORT = process.env.PORT || 5000
+
+app.get("/api/contacts",(req,res)=>{
+    // Send Normal Message to client
+    // res.send("Hello Bikash")
+
+    // Send json response to client
+    // res.json({message:"Get all users"})
+
+    // Send json response to Client with status code
+    res.status(200).json({message:"Get all users"})
+})
 
 
 app.listen(PORT,()=>{
