61cf4984d5d8290c7f0d23c8973280a051b6d503
Bikash <medhi01011997@gmail.com>
2024-07-12 19:48:55 +0530

    PART 12 : Mongoose Schema for Contacts

---
+++ models/contactModel.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const contactSchema = mongoose.Schema({
+    name:{
+        type:String,
+        required:[true,"Please add the contact name"]
+    },
+    email:{
+        type:String,
+        required:[true,"Please add the Email"]
+    },
+    phone:{
+        type:String,
+        required:[true,"Please add the Phone"]
+    }
+},{
+    timestamps : true
+})
+
+module.exports = mongoose.model("Contact",contactSchema)
